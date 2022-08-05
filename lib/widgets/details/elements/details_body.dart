8c6d387b289d5ff616d075916cfa6de98bdbe89b
TalgatBatyrov <90198155+TalgatBatyrov@users.noreply.github.com>
2022-08-05 09:58:22 +0600

    ui:color and size

--- lib/widgets/details/elements/details_body.dart
+++ lib/widgets/details/elements/details_body.dart
@@ -20,12 +20,19 @@ class DetailsBody extends StatelessWidget {
               children: [
                 Container(
                   margin: EdgeInsets.only(top: size.height * 0.3),
-                  height: 500,
+                  padding: EdgeInsets.only(
+                    top: size.height * 0.12,
+                    left: kDefaultPadding,
+                    right: kDefaultPadding,
+                  ),
                   decoration: const BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.only(
                         topLeft: Radius.circular(20),
                         topRight: Radius.circular(20)),
+                  ),
+                  child: Column(
+                    children: [ColorAndSize(product: product)],
                   ),
                 ),
                 ProductTileWithImage(product: product),
@@ -33,6 +40,93 @@ class DetailsBody extends StatelessWidget {
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class ColorAndSize extends StatelessWidget {
+  const ColorAndSize({
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Color'),
+              Row(
+                children: const [
+                  ColorDot(
+                    color: Color(0xFF356C95),
+                    isSelected: true,
+                  ),
+                  ColorDot(color: Color(0xFFF8C078)),
+                  ColorDot(color: Color(0xFFA29B9B)),
+                ],
+              )
+            ],
+          ),
+        ),
+        Expanded(
+          child: RichText(
+            text: TextSpan(
+              style: const TextStyle(color: kTextColor),
+              children: [
+                const TextSpan(
+                  text: 'Size\n',
+                ),
+                TextSpan(
+                  text: '${product.size} cm',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline5
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ColorDot extends StatelessWidget {
+  final Color color;
+  final bool isSelected;
+  const ColorDot({
+    Key? key,
+    required this.color,
+    this.isSelected = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+          top: kDefaultPadding / 4, right: kDefaultPadding / 2),
+      padding: const EdgeInsets.all(2.5),
+      height: 24,
+      width: 24,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(
+          color: isSelected ? color : Colors.transparent,
+        ),
+      ),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: color,
+        ),
       ),
     );
   }
